c2f1b0981d4cc2a08889de4d873c42ccd49f2198
Konstantinos K <raistlindragonl@gmail.com>
2024-08-04 17:44:06 +0300

    Change web-app title and icon

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import "nes.css/css/nes.min.css";
 function App() {
   return (
     <div className="nes-container with-title">
-      <p className="title">Welcome</p>
-      <p>Welcome to NES-classic!</p>
+      <p className="title">BusiNESs Card Generator</p>
+      <p>Welcome to BusiNESs Card Generator!</p>
     </div>
   )
 }
